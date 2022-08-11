4867206d790e8ac25e9f803ee8c08c7ea273e422
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-08-11 11:25:13 -0300

    config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import App from './App.svelte';
+import App from './core/App.svelte'
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
+  target: document.body,
+  props: {
+    name: 'Something',
+  },
+})
 
-export default app;
+export default app
